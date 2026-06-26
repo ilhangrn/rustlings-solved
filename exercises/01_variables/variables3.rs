125a3602312f896377393b83aa75da56c0be9faf
ilhan <ilhangrn@hotmail.com>
2026-06-25 17:32:54 -0700

    fixed 5 cases

--- exercises/01_variables/variables3.rs
+++ exercises/01_variables/variables3.rs
@@ -1,6 +1,12 @@
 fn main() {
     // TODO: Change the line below to fix the compiler error.
-    let x: i32;
+    let x: i32 = Default::default();
+    // or we can use let x :i32 = 0;
+    let y: i32 = 5;
+    let mut z = 6; // will get default type i32
 
+    z = x + y;
     println!("Number {x}");
+    println!("Number {y}");
+    println!("Number {z}");
 }
