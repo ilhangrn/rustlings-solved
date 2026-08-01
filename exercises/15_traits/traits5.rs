80b3ee3246d53dcd8cb97ce831adc910106ee9ed
ilhan <ilhangrn@hotmail.com>
2026-08-01 15:31:36 -0700

    trait bound fix

--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -11,6 +11,8 @@ trait OtherTrait {
 }
 
 struct SomeStruct;
+// when you comment out any impl line gives error
+// because some_func expects T to have both traits implemented.
 impl SomeTrait for SomeStruct {}
 impl OtherTrait for SomeStruct {}
 
@@ -19,12 +21,23 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+// Here we are going to create a function that accepts anything with these traits.
+// becaude we are going call methods from that thraits.
+// <T: SomeTrait + OtherTrait> says that the function `some_func` accepts a generic type
+// `T` that implements both `SomeTrait` and `OtherTrait`. 
+// The function then calls the methods from those traits on the passed item
+fn some_func<T: SomeTrait + OtherTrait>(item: T) -> bool {
+// itrem: T says that we will get one input as named item
+// and it will be type T, 
+// which is a generic type that implements both `SomeTrait` and `OtherTrait`.
+
+// As we know T has them, now we can call the methods from those traits on the passed item.
     item.some_function() && item.other_function()
 }
 
 fn main() {
     // You can optionally experiment here.
+    println!("{}", some_func(SomeStruct));
 }
 
 #[cfg(test)]
