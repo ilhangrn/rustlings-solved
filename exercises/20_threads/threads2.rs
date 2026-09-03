1700f1c527a888a548231f2ec82d6dc8a538babf
ilhan <ilhangrn@hotmail.com>
2026-09-03 15:43:35 -0700

    thread join ops

--- exercises/20_threads/threads2.rs
+++ exercises/20_threads/threads2.rs
@@ -1,8 +1,8 @@
 // Building on the last exercise, we want all of the threads to complete their
 // work. But this time, the spawned threads need to be in charge of updating a
 // shared value: `JobStatus.jobs_done`
-
-use std::{sync::Arc, thread, time::Duration};
+// import random number generator
+use std::{sync::Arc, sync::Mutex, thread, time::Duration};
 
 struct JobStatus {
     jobs_done: u32,
@@ -10,25 +10,39 @@ struct JobStatus {
 
 fn main() {
     // TODO: `Arc` isn't enough if you want a **mutable** shared state.
-    let status = Arc::new(JobStatus { jobs_done: 0 });
+    // `Arc` provides shared ownership of a value, but it doesn't allow mutation.
+    // To have a mutable shared state, you need to use `Mutex` in combination with `Arc`.
+    let status = Arc::new(Mutex::new(JobStatus { jobs_done: 0 }));
 
     let mut handles = Vec::new();
-    for _ in 0..10 {
+    for i in 0..10 {
         let status_shared = Arc::clone(&status);
-        let handle = thread::spawn(move || {
-            thread::sleep(Duration::from_millis(250));
+        let handle = thread::Builder::new()
+            .name(format!("worker-{i}"))
+            .spawn(move || {
+                let binding = thread::current();
+                let thread_name = binding.name().unwrap_or("unknown").to_string();
 
-            // TODO: You must take an action before you update a shared value.
-            status_shared.jobs_done += 1;
+                println!("init {}", thread_name);
+                // if i odd delay negative, if even positive
+                let delay = if i % 2 == 0 { 250} else { 500 };
+                thread::sleep(Duration::from_millis(delay));
+
+                // TODO: You must take an action before you update a shared value.
+                println!("finished {}", thread_name);
+                let mut status = status_shared.lock().unwrap();
+                status.jobs_done += 1;
         });
         handles.push(handle);
     }
 
     // Waiting for all jobs to complete.
     for handle in handles {
-        handle.join().unwrap();
+        println!("Waiting next one to finish");
+        handle.expect("Thread panicked").join().unwrap();
+        println!("joined to main!");
     }
 
     // TODO: Print the value of `JobStatus.jobs_done`.
-    println!("Jobs done: {}", todo!());
+    println!("Jobs done: {}", status.lock().unwrap().jobs_done);
 }
